6bffabc7f036c6849143adae5a13dad1cade9766
girma <girmatarekegn.gi@gmail.com>
2023-06-01 15:00:22 +0300

    Add user session and registration model and controller

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  ROLES = %w[super_admin admin manager editor collaborator].freeze
+
+  ROLES.each do |role_name|
+    define_method "#{role_name}?" do
+      role == role_name
+    end
+  end
+end
